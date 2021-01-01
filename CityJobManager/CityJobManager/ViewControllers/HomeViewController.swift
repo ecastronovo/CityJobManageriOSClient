32c5102a980fdc3d85f4cb4a1c656d2a35f05033
eric <ecastronovo@scu.edu>
2020-12-31 22:34:09 -0800

    Create HomeViewController

---
+++ CityJobManager/CityJobManager/ViewControllers/HomeViewController.swift
@@ -0,0 +1,16 @@
+//
+//  HomeViewController.swift
+//  CityJobManager
+//
+//  Created by Eric Castronovo on 12/31/20.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
